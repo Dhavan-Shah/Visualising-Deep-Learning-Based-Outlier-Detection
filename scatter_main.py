04c689c134dfef1f57de695f1443c17586404333
ccocco1 <105175431+ccocco1@users.noreply.github.com>
2022-09-27 12:49:42 +0100

    Update scatter_main.py

--- scatter_main.py
+++ scatter_main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def home():
     x_value = [1,2,3,4,5,6,7,8,9,10]
     y_value = [5,3,2,1,3,2,3,9,10,2] 
-    return render_template('index.html',x_value=x_value,y_value=y_value)
+    return render_template('scatter_index.html',x_value=x_value,y_value=y_value)
 
 if __name__ == "__main__":
     app.run(debug=True)
