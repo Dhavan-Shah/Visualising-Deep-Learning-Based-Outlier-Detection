f4c4d40e6100ad8cbc39e62a35a178afdf963aa8
ccocco1 <105175431+ccocco1@users.noreply.github.com>
2022-10-06 22:51:13 +0200

    Update arxiv_articles_UMAP.py

--- datasets/arxiv_articles_UMAP.py
+++ datasets/arxiv_articles_UMAP.py
@@ -1,3 +1,5 @@
+#2.2 Streaming and Progressive Visualization
+
 import pandas as pd
 url = 'https://raw.githubusercontent.com/ChenXin360104/ProgressivePyramidBasedSampling/release/data/arxiv_articles_UMAP.csv'
 data = pd.read_csv(url,header=None)
