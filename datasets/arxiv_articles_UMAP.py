771adb5fecd6a4eac20f43f4b662cef79521bf05
ccocco1 <105175431+ccocco1@users.noreply.github.com>
2022-10-06 22:42:08 +0200

    Create arxiv_articles_UMAP.py

---
+++ datasets/arxiv_articles_UMAP.py
@@ -0,0 +1,3 @@
+import pandas as pd
+url = 'https://raw.githubusercontent.com/ChenXin360104/ProgressivePyramidBasedSampling/release/data/arxiv_articles_UMAP.csv'
+data = pd.read_csv(url,header=None)
